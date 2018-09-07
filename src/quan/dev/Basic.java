837d984718eb7e21cbbb9d5213ad1836e47705bf
TranHongQuan <quantran.130798@gmail.com>
2018-09-07 10:52:58 +0700

    Hoan thanh tinh tong hieu tich thong

---
+++ src/quan/dev/Basic.java
@@ -0,0 +1,15 @@
+package quan.dev;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 6;
+		int b = 3;
+		System.out.println("Tong a va b la: "+(a + b));
+		System.out.println("Hieu a va b la: "+(a - b));
+		System.out.println("Tich a va b la: "+(a * b));
+		System.out.println("Thuong a va b la: "+(a / b));
+	}
+
+}
