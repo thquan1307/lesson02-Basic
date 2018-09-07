b82f66a064b87d853cb65e8307b7767156c401e6
TranHongQuan <quantran.130798@gmail.com>
2018-09-07 11:02:40 +0700

    Hoan thanh giai phuong trinh bac hai

--- src/quan/dev/Basic.java
+++ src/quan/dev/Basic.java
@@ -4,12 +4,28 @@ public class Basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 6;
-		int b = 3;
-		System.out.println("Tong a va b la: "+(a + b));
-		System.out.println("Hieu a va b la: "+(a - b));
-		System.out.println("Tich a va b la: "+(a * b));
-		System.out.println("Thuong a va b la: "+(a / b));
+		double a = 1;
+		double b = 6;
+		double c = 2;
+		double delta = (b*b) - (4*a*c);
+		double x1, x2;
+		
+		if (delta < 0){
+			System.out.println("Phuong trinh vo nghiem");
+		}
+		
+		else if (delta > 0){
+			x1 = (-b+Math.sqrt(delta)) / (2*a);
+			x2 = (-b-Math.sqrt(delta)) / (2*a);
+			System.out.println("x1 = " + x1);
+			System.out.println("x2 = " + x2);
+		}
+		
+		else if (delta == 0) {
+			x1 = -b / (2*a);
+			System.out.println("Phuong trinh co nghiem kep x = " + x1);
+		}
+		
 	}
 
 }
