b9ecca2e1f3b2734a1cc02e9253c80f2ecafa8b9
TranHongQuan <quantran.130798@gmail.com>
2018-09-09 10:56:09 +0700

    Tong 100 so dau tien su dung while_loop

--- src/quan/dev/Basic.java
+++ src/quan/dev/Basic.java
@@ -4,9 +4,11 @@ public class Basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 0;
 		int tong = 0;
-		for(int i = 1; i <= 100; i++) {
+		while(i <= 100) {
 			tong = tong + i;
+			i++;
 		}
 		System.out.println("Tong 100 so dau tien la: "+tong);
 	}
